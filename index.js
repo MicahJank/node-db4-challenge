6933bfdf521d405a56c0da8161ee6e76887d06d0
MicahJank <MicahJank@users.noreply.github.com>
2019-11-21 23:40:26 -0600

    added a logger middleware to the server.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
     console.log(`Listenineth on thee porteth that is atop thee number ${PORT}`);
-})
+});
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+// const recipeRouter = require('./recipes/recipeRouter.js');
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use(logger);
+server.use(cors());
+// server.use('/api/recipes', recipeRouter);
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Welcome to the Recipies API!! BOOOM!</h2>`)
+});
+
+function logger(req, res, next) {
+    console.log({
+        request_method: req.method,
+        request_url: req.url,
+        timestamp: Date().toString()
+    });
+    next();
+};
+
+
+module.exports = server;
